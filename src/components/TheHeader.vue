2e06a91750d0019bdfd086cab5fafc06a1f21c0a
LucaSk-x <lucapiccinno64@gmail.com>
2022-06-29 15:54:55 +0200

    first

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <div class="nav-bar px-5">
+            <div class="d-flex justify-content-between align-items-center">
+                <div class="logo">
+                    <img class="w-100" src="../../public/img/netflix-logo.png" alt="">
+                </div>
+                <div class="search">
+                    <div class="input-group m-3">
+                        <input type="text" class="form-control" placeholder="Titoli, persone, generi" aria-label="Recipient's username" aria-describedby="button-addon2" v-model="searchText">
+                        <button class="btn btn-outline-secondary" type="button" id="button-addon2">Search</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+/*     import {state} from "../store"
+ */
+export default {
+    data: {
+        return {
+            searchText: "",
+        },
+    },
+    methods: {
+        
+    }
+
+}
+</script>
+
+<style lang="scss">
+    .nav-bar {
+        background-color: #000000;
+        height: 9vh;
+        .logo {
+            width: 50px
+        }
+    }
+</style>
---
+++ src/components/TheMain.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <div class="main">
+            <div class="container">
+                
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import {state} from '../store'
+
+export default {
+    computed: {
+        movieList() {
+            return state.movieList;
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+    .main {
+        background-color: #141414;
+        height: 91vh;
+    }
+</style>
